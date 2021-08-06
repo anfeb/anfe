400ea49e6c1ee77f9a147bc9375fdb661a58b622
anfebnBotz <88554978+anfebnBotz@users.noreply.github.com>
2021-08-07 00:43:30 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text, isMods, isOwner }) => {
         m.reply(`Berhasil join grup ${res.gid}`)
     } else {
         for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) m.reply('*dari:* ' + m.sender.split('@')[0] + '\n*Link:* ' + link, jid)
-        m.reply('Sedang di process Owner')
+        m.reply('Only Premium User Chat Owner')
     }
 }
 handler.help = ['join [chat.whatsapp.com]']
