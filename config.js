311a0d7096b861d7b0e108a650350868c6fb57eb
anfebnBotz <88554978+anfebnBotz@users.noreply.github.com>
2021-08-07 00:35:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
-global.owner = ['6281390658325', '6282256115584', '6285713964963']// Put your number here
-global.mods = ['13479805233'] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.linkGC = ['https://chat.whatsapp.com/K2GdecBQiHaKjMiQ9bhrNS', 'https://chat.whatsapp.com/LvK86OH4nWyHUAWJS9tNtw']
+global.owner = ['6287772343860', '6285924341386', '6285349896689']// Put your number here
+global.mods = ['627772343860'] // Want some help?
+global.prems = ['6285157160906'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
