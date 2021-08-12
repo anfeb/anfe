c596def945ad61ee37fada4e28904d8fd77bd5c4
anfebnBotz <88554978+anfebnBotz@users.noreply.github.com>
2021-08-12 22:20:50 +0700

    Add files via upload

---
+++ plugins/button.js
@@ -0,0 +1,33 @@
+let { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m, { conn }) => {
+const buttons = [
+{buttonId: 'id1', buttonText: {displayText: 'Promosi Bot'}, type: 1}, 
+{buttonId: 'id2', buttonText: {displayText: '💻Owner anfebnBotz 💻'}, type: 1}, 
+{buttonId: 'id3', buttonText: {displayText: '⚠ SYARAT & KETENTUAN ⚠'}, type: 1}
+]
+const buttonMessage = {
+    contentText: "HALO KAK\nSaya anfebnBotz\nPilih Menu Di Bawah Yaa\nDan Jangan Lupa Baca Dibawah ini\n\nSpam = Banned\nTelp = Blok", 
+    footerText: 'Powered By Baileys\nCreated anfebnBotz By anfebn', 
+    buttons: buttons,
+    headerType: 1
+}
+conn.sendMessage(m.chat, buttonMessage, MessageType.buttonsMessage)
+}
+
+handler.help = ['menu']
+handler.tags = ['jdhsnaj'] 
+handler.command = /^(menu)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+handler.register = true
+
+handler.fail = null
+handler.exp = 20
+
+module.exports = handler
