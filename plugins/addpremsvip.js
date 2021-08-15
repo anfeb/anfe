66893ddc98bfa2b6f1174efda6d37bf55cb498fa
anfeb <88554978+anfeb@users.noreply.github.com>
2021-08-15 23:39:37 +0700

    addprems.js
    Ya

--- plugins/addpremsvip.js
+++ plugins/addpremsvip.js
@@ -5,15 +5,15 @@ let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
   if (!who) throw 'Tag salah satu lah,dan masukkan nomor untuk di verivikasi !'
-  // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
+  if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.DATABASE._data.chats[m.chat].premium = true
   var nomor = m.sender
     m.reply(`*Done berhasil added User✅*\n\n*Nomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\n*Expired:* Unlimited\n*Thanks For Added Premium !*`)
-  // } else m.reply('Ada nomor host disini...')
+  } else m.reply('Ada nomor host disini...')
 }
-handler.help = ['addprems vip<nomor>']
+handler.help = ['addprems <nomor>']
 handler.tags = ['owner']
-handler.command = /^addpremsvip$/i
+handler.command = /^addprems$/i
 handler.rowner = true
 
 module.exports = handler
